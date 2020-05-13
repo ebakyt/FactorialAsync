9acd6adbf5ce356016acc8bb1887e214ff9eff2a
Bakıtbek Eşmurzayev <bakitbek.esmurzayev@siskon.com>
2020-05-13 13:10:54 +0300

    Add project files.

---
+++ FactorialAsync/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FactorialAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine(Factorial(5));     // = 120
+
+			Console.ReadKey();
+		}
+
+
+		public static async Task<double> Factorial(double x)
+		{
+			if (x < 1)
+			{
+				return 1;
+			}
+
+			if (x == 1)
+			{
+				return 1;
+			}
+			else
+			{
+				return x * await Factorial(x - 1);
+			}
+		}
+
+	}
+}
